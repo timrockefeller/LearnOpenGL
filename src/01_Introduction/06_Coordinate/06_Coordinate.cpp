f924d0dc896b47975fba6749f44e9ec4a7c7bfb1
kitekii <tim720719@163.com>
2019-08-25 13:28:01 +0800

    01_06:done

---
+++ src/01_Introduction/06_Coordinate/06_Coordinate.cpp
@@ -0,0 +1,138 @@
+#include <LOGL/Glfw.h>
+#include <LOGL/Shader.h>
+#include <LOGL/Texture.h>
+using namespace KTKR;
+using namespace LOGL;
+using namespace std;
+int main(int argc, char const* argv[]) {
+    const string title = "01_05: Coordinate System";
+    const uint32_t width = 800;
+    const uint32_t height = 600;
+    Glfw::getInstance()->Init(width, height, title);
+
+    // objects
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  //
+        0.5f,  -0.5f, -0.5f, 1.0f, 0.0f,  //
+        0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,  //
+        0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,  //
+        -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f,  //
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  //
+
+        -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,  //
+        0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,  //
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  //
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  //
+        -0.5f, 0.5f,  0.5f,  0.0f, 1.0f,  //
+        -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,  //
+
+        -0.5f, 0.5f,  0.5f,  1.0f, 0.0f,  //
+        -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f,  //
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  //
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  //
+        -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,  //
+        -0.5f, 0.5f,  0.5f,  1.0f, 0.0f,  //
+
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  //
+        0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,  //
+        0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,  //
+        0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,  //
+        0.5f,  -0.5f, 0.5f,  0.0f, 0.0f,  //
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  //
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  //
+        0.5f,  -0.5f, -0.5f, 1.0f, 1.0f,  //
+        0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,  //
+        0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,  //
+        -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,  //
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  //
+
+        -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f,  //
+        0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,  //
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  //
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  //
+        -0.5f, 0.5f,  0.5f,  0.0f, 0.0f,  //
+        -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f   //
+    };
+
+    unsigned int VAO, VBO;
+    {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+                     GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                              (void*)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                              (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+    }
+
+    // textures
+    Texture texture1("assets/textures/container.jpg"),
+        texture2("assets/textures/awesomeface.png", true);
+
+    texture1.setUnit(0);
+    texture2.setUnit(1);
+    // shaders
+    Shader* shader =
+        new Shader("src/01_Introduction/06_Coordinate/vertex.vs",
+                   "src/01_Introduction/06_Coordinate/frag_texture.fs");
+    shader->Use();
+    shader->Use();
+    shader->setInt("texture1", 0);
+    shader->setInt("texture2", 1);
+
+    // coordinate
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
+    glm::mat4 model = glm::mat4(1);
+    model =
+        glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    glm::mat4 view = glm::mat4(1);
+    view = glm::translate(
+        view, glm::vec3(0.0f, 0.0f,
+                        -3.0f));  // 注意：世界物体位移定义为相机位移的反方向。
+    glm::mat4 projection = glm::mat4(1);
+    projection = glm::perspective(glm::radians(45.0f), 1.0f * width / height,
+                                  0.1f, 100.0f);
+
+    shader->setMat4f("model", model);
+    shader->setMat4f("view", view);
+    shader->setMat4f("projection", projection);
+
+    glEnable(GL_DEPTH_TEST);
+
+    // loop
+    OpQueue opList;
+    opList << [&]() { glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); }
+           << Glfw::getInstance()->_startOp <<
+        [&]() {
+            glBindVertexArray(VAO);
+            for (unsigned int i = 0; i < 10; i++) {
+                glm::mat4 model = glm::mat4(1);
+                model = glm::translate(model, cubePositions[i]);
+                float angle = (i%3==0?(float)glfwGetTime():1.0f) * 20.0f * (i + 1);
+                model = glm::rotate(model, glm::radians(angle),
+                                    glm::vec3(1.0f, 0.3f, 0.5f));
+                shader->setMat4f("model", model);
+
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
+        } << Glfw::getInstance()->_endOp;
+    Glfw::getInstance()->Run(&opList);
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    return 0;
+}
---
+++ src/01_Introduction/06_Coordinate/note.md
@@ -0,0 +1,16 @@
+对我们来说比较重要的总共有5个不同的坐标系统：
+
+- 局部空间(Local Space，或者称为物体空间(Object Space))
+- 世界空间(World Space)
+- 观察空间(View Space，或者称为视觉空间(Eye Space))
+- 裁剪空间(Clip Space)
+- 屏幕空间(Screen Space)
+
+$$
+V_{clip}=M_{projection}⋅M_{view}⋅M_{model}⋅V_{local}
+$$
+
+我们想要在场景里面稍微往后移动，以使得物体变成可见的（当在世界空间时，我们位于原点(0,0,0)）。要想在场景里面移动，先仔细想一想下面这个句子：
+
+>  将摄像机向后移动，和将整个场景向前移动是一样的。
+
