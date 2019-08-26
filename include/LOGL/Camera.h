54961fed6483db3973b10ef8dad88e4e8d4df0c9
kitekii <tim720719@163.com>
2019-08-26 09:42:54 +0800

    add camera class

---
+++ include/LOGL/Camera.h
@@ -0,0 +1,79 @@
+#pragma once
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace LOGL {
+enum Camera_Movement { CAM_FORWARD, CAM_BACKWARD, CAM_LEFT, CAM_RIGHT };
+class Camera {
+   public:
+    // Constructor with vectors
+    Camera(float rationWH,
+           glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
+           glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+           float yaw = YAW,
+           float pitch = PITCH);
+    // Constructor with scalar values
+    /*
+    Camera(float posX,
+           float posY,
+           float posZ,
+           float upX,
+           float upY,
+           float upZ,
+           float yaw,
+           float pitch);
+    */
+
+    // Returns the view matrix calculated using Euler Angles and the LookAt
+    // Matrix
+    glm::mat4 GetViewMatrix();
+
+    // Returns the projection matrix calculated using Zoom
+    glm::mat4 GetProjectionMatrix();
+
+    // Processes input received from any keyboard-like input system. Accepts
+    // input parameter in the form of camera defined ENUM (to abstract it from
+    // windowing systems)
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+
+    // Processes input received from a mouse input system. Expects the offset
+    // value in both the x and y direction.
+    void ProcessMouseMovement(float xoffset,
+                              float yoffset,
+                              GLboolean constrainPitch = true);
+
+    // Processes input received from a mouse scroll-wheel event. Only requires
+    // input on the vertical wheel-axis
+    void ProcessMouseScroll(float yoffset);
+
+   private:
+    // Default camera values
+    static const float YAW;
+    static const float PITCH;
+    static const float SPEED;
+    static const float SENSITIVITY;
+    static const float ZOOM;
+
+    // Camera Attributes
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+
+    // Euler Angles
+    float Yaw;
+    float Pitch;
+
+    // Camera options
+    float MovementSpeed;
+    float MouseSensitivity;
+    float Zoom;
+    float RationWH;
+
+    // Calculates the front vector from the Camera's (updated) Euler Angles
+    void updateCameraVectors();
+};
+
+}  // namespace LOGL
