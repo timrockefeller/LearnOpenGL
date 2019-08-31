22a43cdf82e0ad23fb6f2d6e616183fc553b7a62
kitekii <tim720719@163.com>
2019-08-31 16:51:26 +0800

    feat : more event key

--- include/Util/EventListener.h
+++ include/Util/EventListener.h
@@ -5,6 +5,8 @@
 #include <map>
 namespace KTKR {
 class EventListener : public Singleton<EventListener> {
+    friend class Singleton<EventListener>;
+
    public:
     enum Event_Type {
         // KEY = 0x00000000 ~ 0x0000FFFF
@@ -13,13 +15,17 @@ class EventListener : public Singleton<EventListener> {
         KEYBOARD_REPEAT = 0x00020000,
         KEYBOARD_RELEASE = 0x00040000,
         KEYBOARD = KEYBOARD_PRESS | KEYBOARD_REPEAT | KEYBOARD_RELEASE,
-
+        MOUSE_SCROLL = 0x00080000,
+        MOUSE_MOVE = 0x00080001,
+        WINDOW_ZOOM = 0x00080002,
     };
 
-    EventListener& bind(size_t event, const std::function<void()>& op);
+    EventListener* bind(size_t event, const std::function<void()>& op);
     void response(size_t event);
 
    private:
+    EventListener() = default;
+    ~EventListener() = default;
     EventListener(const EventListener&) = delete;
     EventListener& operator=(const EventListener&) = delete;
     std::map<size_t, Ptr<OpQueue>> listenerList;
--- src/lib/Util/EventListener/EventListener.cpp
+++ src/lib/Util/EventListener/EventListener.cpp
@@ -1,13 +1,18 @@
 #include <Util/EventListener.h>
+#include <Util/OpQueue.h>
+#include <memory>
 using namespace KTKR;
 using namespace std;
 
-EventListener& EventListener::bind(size_t event,
+EventListener* EventListener::bind(size_t event,
                                    const std::function<void()>& op) {
-    if (listenerList.find(event) == listenerList.end())
-        listenerList[event] = OpQueue::toPtr(new OpQueue);
+    if (listenerList.find(event) == listenerList.end()) {
+        OpQueue* opq = new OpQueue();
+        listenerList[event] =
+            std::shared_ptr<OpQueue>(opq, [&](OpQueue* op_) { delete op_; });
+    }
     listenerList[event]->operator<<(op);
-    return *this;
+    return this;
 }
 
 void EventListener::response(size_t event) {
