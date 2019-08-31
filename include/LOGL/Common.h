5a12ac7234ddcb3e618d4b454c23fc5c330f6467
kitekii <tim720719@163.com>
2019-08-31 16:51:42 +0800

    feat : common define

---
+++ include/LOGL/Common.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+namespace LOGL {
+// Common Define
+
+/// event bridge ///
+
+// mouse scroll x offset
+static std::string strMouseScrollX = "mouseScroll_xoffset";
+// mouse scroll y offset
+static std::string strMouseScrollY = "mouseScroll_yoffset";
+// mouse position x offset
+static std::string strMousePosX = "mouseScroll_xoffset";
+// mouse position y offset
+static std::string strMousePosY = "mouseScroll_yoffset";
+
+}  // namespace LOGL
