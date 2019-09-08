be61dfafa066504d56fb17f31822904e43a25fe4
kitekii <tim720719@163.com>
2019-09-08 22:01:40 +0800

    add Mesh class

---
+++ include/LOGL/Mesh.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <LOGL/Shader.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <string>
+#include <vector>
+#include <glad/glad.h>
+namespace LOGL {
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+struct Texture {
+    unsigned int ID;
+    std::string type;
+};
+
+class Mesh {
+   public:
+    // mesh data
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+    Mesh(std::vector<Vertex> vertices,
+         std::vector<unsigned int> indices,
+         std::vector<Texture> textures);
+    void Draw(Shader shader);
+   private:
+    unsigned int VAO, VBO, EBO;
+    void setupMesh();
+};
+
+}  // namespace LOGL
