5e96c80501d379eba50721396dd962c137ba0380
kitekii <tim720719@163.com>
2019-09-08 22:55:19 +0800

    add Model class

---
+++ include/LOGL/Model.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <assimp/scene.h>
+#include <string>
+#include <vector>
+
+#include <LOGL/Mesh.h>
+#include <LOGL/Shader.h>
+namespace LOGL {
+
+class Model {
+   public:
+    Model(char* path) { loadModel(path); }
+    void Draw(Shader shader);
+
+   private:
+    /*  模型数据  */
+    std::vector<Mesh> meshes;
+    std::string directory;
+    /*  函数   */
+    void loadModel(std::string path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial* mat,
+                                              aiTextureType type,
+                                              std::string typeName);
+};
+}  // namespace LOGL
---
+++ src/lib/LOGL/Model/Model.cpp
@@ -0,0 +1,119 @@
+#include <LOGL/Model.h>
+#include <assimp/postprocess.h>
+#include <assimp/Importer.hpp>
+#include <iostream>
+using namespace LOGL;
+using namespace std;
+
+void Model::Draw(Shader shader) {
+    for (unsigned int i = 0; i < meshes.size(); i++) {
+        meshes[i].Draw(shader);
+    }
+}
+
+void Model::loadModel(string path) {
+    Assimp::Importer importer;
+    const aiScene* scene = importer.ReadFile(
+        path,                  // 下面传入post process(预处理)的方法
+        aiProcess_Triangulate  // 若模型不是全由三角形构成，需事先将所有图元转变为三角形
+            | aiProcess_FlipUVs  // 翻转纹理y坐标
+        // 同时还有其他的：(参见readme.md)
+        // aiProcess_GenNormals：如果模型不包含法向量的话，就为每个顶点创建法线。
+        // aiProcess_SplitLargeMeshes：将比较大的网格分割成更小的子网格，如果你的渲染有最大顶点数限制，只能渲染较小的网格，那么它会非常有用。
+        // aiProcess_OptimizeMeshes：和上个选项相反，它会将多个小网格拼接为一个大的网格，减少绘制调用从而进行优化。
+    );
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE ||
+        !scene->mRootNode) {
+        cout << "ERROR::ASSIMP::" << importer.GetErrorString() << endl;
+        return;
+    }
+    directory = path.substr(0, path.find_last_of('/'));
+
+    processNode(scene->mRootNode, scene);
+}
+
+void Model::processNode(aiNode* node, const aiScene* scene) {
+    // 处理节点所有的网格（如果有的话）
+    for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+        aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+        meshes.push_back(processMesh(mesh, scene));
+    }
+    // 接下来对它的子节点重复这一过程
+    for (unsigned int i = 0; i < node->mNumChildren; i++) {
+        processNode(node->mChildren[i], scene);
+    }
+}
+
+Mesh Model::processMesh(aiMesh* mesh, const aiScene* scene) {
+    vector<Vertex> vertices;
+    vector<Texture> textures;
+    vector<unsigned int> indices;
+
+    // vertices
+    for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
+        Vertex vertex;
+        glm::vec3 vector;
+        vector.x = mesh->mVertices[i].x;
+        vector.y = mesh->mVertices[i].y;
+        vector.z = mesh->mVertices[i].z;
+
+        vertex.Position = vector;
+
+        glm::vec3 normal;
+        normal.x = mesh->mNormals[i].x;
+        normal.y = mesh->mNormals[i].y;
+        normal.z = mesh->mNormals[i].z;
+
+        vertex.Normal = normal;
+
+        // glm::vec2 texCoords;
+        // texCoords.x = mesh->mTextureCoords[i].x;
+        // texCoords.y = mesh->mTextureCoords[i].y;
+        if (mesh->mTextureCoords[0])  // 网格是否有纹理坐标？
+        {
+            glm::vec2 vec;
+            vec.x = mesh->mTextureCoords[0][i].x;
+            vec.y = mesh->mTextureCoords[0][i].y;
+            vertex.TexCoords = vec;
+        } else
+            vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+        vertices.push_back(vertex);
+    }
+
+    // indices
+    for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
+        aiFace face = mesh->mFaces[i];
+        for (unsigned int j = 0; j < face.mNumIndices; j++)
+            indices.push_back(face.mIndices[j]);
+    }
+
+    // textures
+    if (mesh->mMaterialIndex >= 0) {
+        aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+        vector<Texture> diffuseMaps = loadMaterialTextures(
+            material, aiTextureType_DIFFUSE, "texture_diffuse");
+        textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+        vector<Texture> specularMaps = loadMaterialTextures(
+            material, aiTextureType_SPECULAR, "texture_specular");
+        textures.insert(textures.end(), specularMaps.begin(),
+                        specularMaps.end());
+    }
+
+    return Mesh(vertices, indices, textures);
+}
+
+vector<Texture> Model::loadMaterialTextures(aiMaterial *mat, aiTextureType type, string typeName)
+{
+    vector<Texture> textures;
+    for(unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+    {
+        aiString str;
+        mat->GetTexture(type, i, &str);
+        Texture texture;
+        texture.ID = TextureFromFile(str.C_Str(), directory);
+        texture.type = typeName;
+        texture.path = str;
+        textures.push_back(texture);
+    }
+    return textures;
+}
