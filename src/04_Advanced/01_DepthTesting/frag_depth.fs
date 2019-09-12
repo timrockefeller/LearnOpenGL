ad02daa5a7e682f1868911bbcd54357039247927
kitekii <tim720719@163.com>
2019-09-12 21:03:37 +0800

    加点空气透视

--- src/04_Advanced/01_DepthTesting/frag_depth.fs
+++ src/04_Advanced/01_DepthTesting/frag_depth.fs
@@ -1,8 +1,12 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec2 TexCoords;
+
+uniform sampler2D texture1;
+
 float near = 0.1; 
-float far  = 100.0; 
+float far  = 60.0; 
 
 float LinearizeDepth(float depth) 
 {
@@ -13,5 +17,5 @@ float LinearizeDepth(float depth)
 void main()
 {             
     float depth = LinearizeDepth(gl_FragCoord.z) / far; // 为了演示除以 far
-    FragColor = vec4(vec3(depth), 1.0);
+    FragColor = vec4(depth*vec3(0.35,0.64,1.0) + texture(texture1, TexCoords).rgb, 1.0);
 }
